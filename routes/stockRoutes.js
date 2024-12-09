02573930869b6e93a3828ffc8bef0bd783355444
Ramon <ju4nrsuarez@gmail.com>
2024-12-09 06:13:00 -0300

    borre log

--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -272,41 +272,6 @@ router.delete('/deleteAll', (req, res) => {
     });
   });
 
-  router.post('/login', (req, res) => {
-    const { nombre, password } = req.body;
   
-    if (!nombre || !password) {
-      return res.status(400).json({ error: 'Debe proporcionar un nombre y una contraseña.' });
-    }
-  
-    // Consulta para verificar el usuario
-    const sqlQuery = `
-      SELECT * FROM usuarios
-      WHERE nombre = ? AND password = ?
-    `;
-  
-    pool.query(sqlQuery, [nombre, password], (err, results) => {
-      if (err) {
-        console.error('Error al consultar la base de datos:', err);
-        return res.status(500).json({ error: 'Error al consultar la base de datos.' });
-      }
-  
-      if (results.length === 0) {
-        // Usuario no encontrado o credenciales incorrectas
-        return res.status(401).json({ error: 'Credenciales incorrectas.' });
-      }
-  
-      // Usuario autenticado con éxito
-      const user = results[0];
-      res.status(200).json({
-        message: 'Inicio de sesión exitoso.',
-        user: {
-          id: user.id,
-          nombre: user.nombre,
-          rol: user.rol,
-        },
-      });
-    });
-  });
   
   module.exports = router;
